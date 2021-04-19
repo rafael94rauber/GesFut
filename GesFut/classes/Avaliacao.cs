c78f14630bb168e4d667a41bb7ec4ebeb1c7be87
DESKTOP-18SMIHL\bruno <brunofuhr_@hotmail.com>
2021-04-19 19:27:17 -0300

    adding avaliacaoDesempenho classes

---
+++ GesFut/classes/Avaliacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GesFut
+{
+    public abstract class Avaliacao
+    {
+        public int CodigoAvaliacao { get; }
+        public int CodigoAtleta  { get;set; }
+        public DateTime DataAvaliacao { get; set; }
+        public int UsuarioResponsavel { get; set; }
+        public double Peso { get; set; }
+        public abstract String GetInsert();
+        
+    }
+}
---
+++ GesFut/classes/AvaliacaoDesempenho.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GesFut
+{
+    class AvaliacaoDesempenho : Avaliacao
+    {
+        public DateTime TempoJogo { get; set; }
+        public double PercentualGordura { get; set; }
+        public double VelocidadeMedia { get; set; }
+
+        public override String GetInsert()
+        {
+            return $"INSERT INTO AVALIACAO_DESEMPENHO (codigo_atleta, data_avaliacao, usuario_responsavel, peso, tempo_jogo, percentual_gordura, velocidade_media) " +
+                   $"values ({CodigoAtleta}, {DataAvaliacao.ToString("yyyy-MM-dd")}, {UsuarioResponsavel}, {Peso}, {TempoJogo.ToString("mm:HH:ss")}, {PercentualGordura}, {VelocidadeMedia}) ";
+        }
+    }
+}
