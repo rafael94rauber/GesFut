160198284217cd8d4cf6ef91f5dbf80610768b73
DESKTOP-18SMIHL\bruno <brunofuhr_@hotmail.com>
2021-04-19 20:10:27 -0300

    inclusão de classe e ajuste nos formularios

---
+++ GesFut/classes/AvaliacaoMedica.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GesFut
+{
+    class AvaliacaoMedica : Avaliacao
+    {
+        public double TemperaturaCorporal { get; set; }
+        public String Pressao { get; set; }
+        public int BatimentosCardiacos { get; set; }
+        public String Alergias { get; set; }
+        public Boolean Lesao { get; set; }
+    }
+}
